38c54c52425c1cbc557cda6b0179145600c8bff9
MS <jad@clevi.ai>
2025-07-14 14:10:44 +0900

    ㅇㅇ

---
+++ 2/src/App.tsx
@@ -0,0 +1,12 @@
+import Tetris from "./components/Tetris";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 40 }}>
+      <h1>React Tetris</h1>
+      <Tetris />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2/vite.config.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/stss/1/dist', // ← 실제 배포 경로에 맞게 수정!
+});
